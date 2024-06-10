a31f20e33768ff250b7544946c0f89bfaf22507a
SmartChain-AI <auscryptocurrency@gmail.com>
2024-06-10 18:41:46 +1000

    edits

--- src/pages/api/dappername.js
+++ src/pages/api/dappername.js
@@ -1,28 +1,32 @@
-export default async function handler(req, res) {
+  export default async function handler(req, res) {
 
-  const proxyUrl = 'http://104.131.4.61:8080/';
-  const url_dappername = 'https://graphql-api.meetdapper.com/graphql?GetPublicAccountWithAvatar';
+    const exampleusername = "0x9ca2ddd25b5fbd4b"
+    const proxyUrl = 'http://104.131.4.61:8080/crz/';
 
-  const dappername_requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'Mozilla Firefox 5.0'
-    },
-    body: JSON.stringify(req.body),
-  };
-
-  const response = fetch(url_dappername, dappername_requestOptions)
-    .then((response) => {
-      console.info(response.toString())
-    //  response.json()
-    })
-    .then((data) => {
-      console.log('test')
-
-      console.info(data)
-      res.status(200).json(data)
-    }).catch(console.error);
-
-  const done = await response
-}
+    const url_dappername = 'https://graphql-api.meetdapper.com/graphql?GetPublicAccountWithAvatar';
+  
+    const pload = {
+       "operationName": "GetPublicAccountWithAvatar",
+       "variables": {
+          "usernameOrFlowAddress": exampleusername
+       },
+       "query": "query GetPublicAccountWithAvatar($usernameOrFlowAddress: String!) {\n  getPublicAccountWithAvatar(usernameOrFlowAddress: $usernameOrFlowAddress) {\n    username\n    flowAddress\n    deactivated\n    avatar {\n      id\n      imageURL\n      name\n      __typename\n    }\n    __typename\n  }\n}",
+      };
+  
+    const trequestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(pload)
+      };
+  
+    const response = fetch(url_dappername, trequestOptions )
+      .then((response) => response.json())
+      .then((data) => {
+        res.status(200).json(data)
+        return
+      }).catch(console.error);
+  
+    const done = await response
+  }
