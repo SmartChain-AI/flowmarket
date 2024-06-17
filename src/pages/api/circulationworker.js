ea54396ac04d7d3dac488314cd26357e400b4265
SmartChain-AI <auscryptocurrency@gmail.com>
2024-06-17 22:38:42 +1000

    edit

--- src/pages/api/circulationworker.js
+++ src/pages/api/circulationworker.js
@@ -1,12 +1,15 @@
-// Grabs a list of all available sets and then gets their burn info etc
-// Push to DB
+// Grabs a list of all available sets and then gets their burn info etc and pushes to DB
 import Bottleneck from "bottleneck";
 
 export default async function circulation(req, res) {
-  
   const { MongoClient, ServerApiVersion } = require('mongodb');
+
   const uri = "mongodb+srv://doadmin:617ViK9vH2Y03xG8@flowmarket-db-7c310bf1.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=flowmarket-db";
   //"+process.env.DB_PW+"
+  const url_set_circ = 'https://market-api.ufcstrike.com/sets/';
+  const url_sets = 'https://market-api.ufcstrike.com/search/sets';
+  let tmparr = []
+
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -19,56 +22,33 @@ export default async function circulation(req, res) {
     try {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
-
-
       //await client.db("flowmarket-db").command( { serverStatus: 1 } ).metrics.apiVersions
+      const session = client.startSession();
+
       console.log("here");
-
-
       await client.db("flowmarket-db").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
-     const session = client.startSession();
-    // await getter(session)
 
+      session.startTransaction();
 
+      const filterx = {
+        'sales.timestamp': {
+          '$gte': '2023-06-16T18:24:05Z'
+        }
+      };
 
-
-     session.startTransaction();
-     //const savingsColl = client.db("admin").collection("FlowMarket");
-     //savingsColl.insertOne({ setID: 'Jane Eyre' }, { session })
-
-     
-
-     const test = {
-      'sales.timestamp': '2023-06-16T19:24:05Z'
-    };
-
-     const filter = {
-      'sales.timestamp': {
-        '$gte': '2023-06-16T18:24:05Z'
-      }
-    };
-
-    const coll = client.db('admin').collection('FlowMarket');
-    const cursor = coll.find(filter);
-    const result = await cursor.toArray();
-   // coll.insert(test)
-console.info(result)
-    await session.endSession();
-    await client.close()
-
+      const coll = client.db('admin').collection('FlowMarket');
+      const cursor = coll.find(filter);
+      const result = await cursor.toArray();
+      await getter(coll)
+      await session.endSession();
     } catch (error) {
-        console.log("An error occurred during the transaction:" + error);
-        await session.abortTransaction();
+      console.log("An error occurred during the transaction:" + error);
+      await session.abortTransaction();
     } finally {
-       console.log("Done");
+      // await client.close()
     }
-    
   }
-
-  const url_set_circ = 'https://market-api.ufcstrike.com/sets/';
-  const url_sets = 'https://market-api.ufcstrike.com/search/sets';
-  const tmparr = []
 
   const limiter = new Bottleneck({
     maxConcurrent: 2,
@@ -98,55 +78,31 @@ console.info(result)
     body: JSON.stringify(post_data_sets),
   };
 
-  async function getter(session) {
+  async function getter(coll) {
     await fetch(url_sets, sets_requestOptions)
       .then((response) => response.json())
       .then((data) => {
-
-
-
-
-
-
-
         data.sets.forEach(element => {
-          //console.info(element)
           limiter.schedule(async () => await fetch(url_set_circ + element.set_id + '/circulation')
             .then((response) => response.json())
             .then((data) => {
-              dbinsert(data)
-          //    console.log(data)
+              const coll = client.db('admin').collection('FlowMarket');
+              const cursor2 = coll.insertOne({ //_id: 375, 
+                item: data.setId, info: data
+              })
+              return
             })
             .catch(console.error)
           )
         })
-
-
-
-
-
-
-
+        return data
       })
-      .finally(() => {
-        console.log('done')
-
-      
-
-        res.status(200).json(tmparr)
+      .finally((data) => {
+        return data
       })
       .catch(console.error)
   }
 
-  async function dbinsert(dataz) {
-    
-//
-  }
-
-    
-
   run().catch(console.dir);
   res.status(200).json(tmparr)
-
-
 }
